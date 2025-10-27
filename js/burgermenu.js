b6ec15aea6d318927770844b9d984ab564080767
JustinMoua <Hmuas11037@gmail.com>
2025-10-27 15:35:15 -0500

    Made UI hover changes
    Added hovering to clickable buttons. Changed some images.

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -22,24 +22,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  const body = document.body;
-  const themeSwitch = document.getElementById("theme-switch");
-  const themeSwitchMobile = document.getElementById("theme-switch-mobile");
-
-  // Load stored theme
-  const currentTheme = localStorage.getItem("theme");
-  if (currentTheme === "dark") body.classList.add("dark-mode");
-
-  function toggleTheme() {
-    body.classList.toggle("dark-mode");
-    localStorage.setItem(
-      "theme",
-      body.classList.contains("dark-mode") ? "dark" : "light"
-    );
-  }
-
-  themeSwitch?.addEventListener("click", toggleTheme);
-  themeSwitchMobile?.addEventListener("click", toggleTheme);
-});
--- js/darkmode.js
+++ js/darkmode.js
@@ -1,21 +1,33 @@
-let darkmode = localStorage.getItem('darkmode')
+// ==============================
+// DARK MODE TOGGLE SCRIPT
+// ==============================
 
-const themeSwitch = document.getElementById('theme-switch')
+//User's darkmode preference from local storage.
+let darkmode = localStorage.getItem("darkmode");
 
+// Select theme switch buttons (desktop and mobile)
+const themeSwitch = document.getElementById("theme-switch");
+const themeSwitchMobile = document.getElementById("theme-switch-mobile");
+
+// ==============================
+// DARK MODE TOGGLE FUNCTIONS
+// ==============================
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-}
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", "inactive");
+};
+
+if (darkmode === "active") enableDarkmode();
+
+function toggleDarkmode() {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
 }
 
-if (darkmode === "active") enableDarkmode()
-
-themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
-
+themeSwitch?.addEventListener("click", toggleDarkmode);
+themeSwitchMobile?.addEventListener("click", toggleDarkmode);
